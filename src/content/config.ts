8723f8d9dc8f07bf79de92a1b38138d7edb2b254
radenrishwan <radenrishwan@gmail.com>
2024-03-19 13:02:48 +0700

    implememnt markdown content

--- src/content/config.ts
+++ src/content/config.ts
@@ -10,6 +10,8 @@ const blog = defineCollection({
 		pubDate: z.coerce.date(),
 		updatedDate: z.coerce.date().optional(),
 		heroImage: z.string().optional(),
+		author: z.string().optional(),
+		short: z.string()
 	}),
 });
 
